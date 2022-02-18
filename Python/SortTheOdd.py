4beca3d57d7bd4079be83f6574c907daf728d64d
Ihsan <amalaihsanahsanu@gmail.com>
2022-02-18 20:23:37 +0700

    Some

---
+++ Python/SortTheOdd.py
@@ -0,0 +1,15 @@
+
+
+# def sort_array(source_array):
+#     temp = []
+#     for i in range(len(source_array)):
+#         if source_array[i] % 2 != 0:
+#             temp.append(source_array[i])
+#     return temp
+
+# print(sort_array([3, 8, 6, 5, 4]))        
+data = [5, 3, 2, 8, 1, 4]
+testing = [el for el in data if el %2==1]
+odds = iter(sorted(el for el in data if el % 2))
+a = [next(odds) if el % 2 else el for el in data]
+print(testing)
---
+++ Python/delete_nth.py
@@ -0,0 +1,17 @@
+from collections import Counter
+def delete_nth(order, max_e):
+    
+    
+    # temp = 0
+    checker = dict(Counter(order))
+    # arrCheckerValues = list(checker.values())
+    print(checker)
+    for key in checker:
+        if checker[key] >
+
+    # for i in arrCheckerValues:
+    #     if i >=:
+    #         temp+=1
+    # return temp
+
+delete_nth([1,1,3,3,7,2,2,2,2])
