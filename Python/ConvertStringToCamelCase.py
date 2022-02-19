7864a328dbe562512352ddc3590d136f050c3594
Ihsan <amalaihsanahsanu@gmail.com>
2022-02-19 14:52:55 +0700

    Finish Camel Case Problem With Python

--- Python/ConvertStringToCamelCase.py
+++ Python/ConvertStringToCamelCase.py
@@ -1,5 +1,16 @@
+import re
 def to_camel_case(text):
+    # Convert String To List
     a = [char for char in text]
-    print(a)
-    
-to_camel_case("Banana")
+    # Search '-' char after '-' be Upper
+    for i in range(len(a)):
+            if a[i] == '_' or a[i] == '-' and i != (len(a)-1):
+                a[i+1] = a[i+1].upper()
+    # Join the list into String again
+    text = "".join(a)
+    # Remove Char "_" and "-"
+    text = re.sub('[_-]', '', text) 
+    return text
+
+# print( to_camel_case("the-stealth-warrior"))
+print(to_camel_case("the-cat_Is-evil"))
