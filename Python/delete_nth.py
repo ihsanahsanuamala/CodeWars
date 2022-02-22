357e90354c1fb358a5ffa2ade74c20afd664fbfe
Ihsan <amalaihsanahsanu@gmail.com>
2022-02-22 19:28:50 +0700

    Add Likes Cases

--- Python/delete_nth.py
+++ Python/delete_nth.py
@@ -1,9 +1,10 @@
 from collections import Counter
-def delete_nth(order, max_e):
+def delete_nth(order):
     
     
     # temp = 0
     checker = dict(Counter(order))
+    dict_order = {k: v for v, k in enumerate(order)} 
     # arrCheckerValues = list(checker.values())
     # print(checker)
     # for key in checker:
@@ -13,5 +14,12 @@ def delete_nth(order, max_e):
     #     if i >=:
     #         temp+=1
     # return temp
+    print(dict_order)
+    # return checker
+    for k, v in checker.items():
+        print(k,v)    
 
+
+
+# print(delete_nth([1,1,3,3,7,2,2,2,2]))
 delete_nth([1,1,3,3,7,2,2,2,2])
--- Python/likes.py
+++ Python/likes.py
@@ -1,24 +1,20 @@
 def likes(names):
     if names:
-       if len(names) < 4:
-           if len(names) == 1:
-               return f"{names[0]} likes this"
-           elif len(names) == 2:
-               return f"{names[0]} and {names[1]} likes this" 
-           else:
-               return f"{names[0]}, {names[1]} and {names[2]} likes this"
-       else:
-            sisa = len(names)-2
-            return f"{names[0]}, {names[1]} and {sisa} others likes this"       
+            if len(names) < 4:
+                if len(names) == 1:
+                    return f"{names[0]} likes this"
+                elif len(names) == 2:
+                    return f"{names[0]} and {names[1]} likes this" 
+                else:
+                    return f"{names[0]}, {names[1]} and {names[2]} likes this"
+            else:
+                sisa = len(names)-2
+                return f"{names[0]}, {names[1]} and {sisa} others likes this"       
     else:
         return "no one likes this"
 
 # print(likes(["Peter", "Banu", "Suherman", "Aditya", "Rahma"]))
 print(likes(["Jacob", "Alex"]))
 
-# umur = 22
-# print(f'Umur saya Adalah {umur}')
-
-# print(["Peter"])
 
  
