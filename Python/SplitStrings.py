85a7ebf5d747596b7f59f11cf461af81d409b519
Ihsan <amalaihsanahsanu@gmail.com>
2022-04-11 21:02:57 +0700

    Solve Split String With Python

--- Python/SplitStrings.py
+++ Python/SplitStrings.py
@@ -1,6 +1,18 @@
-kata = "Ihsan"
-kata_arr = [ch for ch in kata]
+import textwrap
+kata = "abcde"
 
-print(kata_arr)
-def solution(s):
-    pass  
+def splitStrings(kata):
+    if len(kata) % 2 == 0:
+        return(textwrap.wrap(kata, 2))
+    else:
+        kata+="_"
+        return(textwrap.wrap(kata, 2))
+
+print(splitStrings(kata))
+
+# kata_arr = [ch for ch in kata]
+# if len(kata_arr) % 2 == 0:
+#     for i in 
+# print(kata_arr)
+# def solution(s):
+#     pass  
